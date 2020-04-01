84d992288348380883eb9dc2c068d4c6744f3e0d
Spyros <csd3722@csd.uoc.gr>
2020-04-01 20:57:29 +0300

    arxiko

--- phase3/utilities/quad.c
+++ phase3/utilities/quad.c
@@ -3,13 +3,19 @@
 #define STARTING_SIZE 1024
 
 #define EXTEND 1024
-#define NEW_SIZE (EXTEND * sizeof(quad) + total)
+#define CURR_SIZE (total*sizeof(quad))
+#define NEW_SIZE (STARTING_SIZE*sizeof(quad) + CURR_SIZE)
+/* extern int yylineno; */
 
-quad *quads;
-double total;
+
+
+quad *quads = (quad*) 0;
+unsigned int total = 0 ;
+unsigned int currQuad = 0;
 
 void init_quads()
 {
+    currQuad = 0;
     quads = malloc(sizeof(quad) * STARTING_SIZE);
     total = STARTING_SIZE;
     for (int i = 0; i < STARTING_SIZE; i++)
@@ -19,7 +25,7 @@ void init_quads()
         quads[i].arg2 = NULL;
         quads[i].label = -1;
         quads[i].op = NULL;
-        quads[i].resault = NULL;
+        quads[i].result = NULL;
     }
 
 }
@@ -33,7 +39,6 @@ void extend_quads()
         free(quads);
     }
     quads = p;
-    
     total += EXTEND;
     
 }
@@ -48,14 +53,52 @@ void print_quads()
     }
 }
 
+void print_quads_not_empty()
+{
+    int i = 0;
+    for (i = 0; i < total; i++)
+    {
+        if(quads[i].arg1 !=NULL)
+            printf("%d (%d)\n", quads[i].label, i);
+    }
+}
+
+quad newQuad(iopcode* op , expr* arg1 , expr* arg2 , expr* res)
+{
+    quad tmp;
+    tmp.op = op;
+    tmp.arg1 = arg1;
+    tmp.arg2 = arg2;
+    tmp.result = res;
+    tmp.lineno = 10;
+    /* kati gia to label */
+    return tmp;
+}
+
+
+void emit(iopcode* op , expr* arg1 , expr* arg2 , expr* res)
+{
+    /* ean den ftanei o xwros kanei extend */
+    if(currQuad >= total) extend_quads();
+    /* create a new Quad */
+    quad tmp = newQuad(op , arg1 , arg2 , res);
+    /* add it to array */
+    quads[currQuad] = tmp;
+    /* increse currQuad with 1 */
+    currQuad = currQuad + 1;
+}
+
 int main()
 {
     init_quads();
-    print_quads();
-    extend_quads();
+    expr* db1 = malloc(sizeof(expr));
+    db1->next = NULL;
+    db1->boolConst = 1;
+     
+    emit(ASSIGN , db1 , db1 , db1 );
+    print_quads_not_empty();
     printf("AFTER EXTENTION1---------\n");
-    extend_quads();
-    /* print_quads(); */
-    printf("%f \n" , total);
+
+    printf("%u \n", total);
     return 0;
 }
--- phase3/utilities/quad.h
+++ phase3/utilities/quad.h
@@ -3,31 +3,36 @@
 #include "parserUtilities.h"
 
 
-
+/* tha ta valw me tin seira */
 /* kapoia einai me kefalaio kai alla oxi giati to exoyme lousei me tis deilwseis */
 typedef enum Iopcode {
-    Assign , Mul , Uminus ,
-    Not , if_lesseq , if_greater ,
-    Ret ,  Funcend , Tablegetelem , 
-    Add , Div ,And , if_eq , 
-    if_greatereq , Call , Getretval , 
-    Tablecreate , Tablesetelem ,
-    Sub , Mod , Or,
-    if_noteq , if_less , Param ,
-     Funcstart
+    ASSIGN , ADD , SUB ,
+    MUL , DIV , MOD ,
+    UMINUS , AND , OR, 
+    NOT , IF_EQ ,IF_NOTEQ , IF_LESSEQ , 
+    IF_GREATEREQ , IF_LESS , IF_GREATER , 
+    JUMP, CALL,
+    PARAM , RETURN , GETRETVAL,
+    FUNCSTART , FUNCEND , TABLECREATE ,
+     TABLEGETELEM , TABLESETELEM
 } iopcode;
-
+    
+//ti mporei na einai to expression
 typedef enum expression_template{
     var_ ,                                  
     tableitem_ ,                    /* table item exprastion */
+    
     pfunc_ ,                        /* program Function  */
     lfunc_ ,                        /* library Function */
+    
     arthmexp_ ,                     /* arithmetic expression */
     assignexp_ ,                    /* assign Expression */
     newtable_ ,                     /* new Table expression */
+    
     constnum_ ,                     /* Const number */
     constbool_ ,                    /* const boolean */
     conststring_ ,                  /* const String*/
+   
     nill_
 }expr_t;
 
@@ -45,7 +50,7 @@ typedef struct expression {
 typedef struct Quad{
     /* enum  opcode  */
     iopcode* op;
-    expr* resault;
+    expr* result;
     expr* arg1;
     expr* arg2;
     int label;          /* ???????? */
