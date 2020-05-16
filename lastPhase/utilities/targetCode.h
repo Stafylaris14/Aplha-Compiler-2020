ea13b1e8ee4d6e0560fc27ef2b7d79d43a72c496
ioakim100 <csd3600@csd.uoc.gr>
2020-05-16 18:40:14 +0300

    kwdikas apo deuteri diale3i den itan ilopoiimenew olew oi synartiseis

--- lastPhase/utilities/targetCode.h
+++ lastPhase/utilities/targetCode.h
@@ -1,6 +1,15 @@
 
 #include "quad.h"
 #include <assert.h>
+
+#define AVM_STACKSIZE 4096
+#define AVM_WIPEOUT(m) memset(&(m), 0, sizeof(m))
+#define AVM_TABLE_HASHSIZE 211
+
+typedef struct avm_memcell avm_memcell; //na ta 3anadwwwww
+typedef struct avm_table_bucket avm_table_bucket;
+
+
 typedef enum vmopcode
 {
     assign_v,
@@ -70,6 +79,53 @@ struct userFunc
 };
 
 
+
+typedef struct avm_table {
+  unsigned refCounter;
+  avm_table_bucket* strIndexed[AVM_TABLE_HASHSIZE];
+  avm_table_bucket* numIndexed[AVM_TABLE_HASHSIZE];
+  unsigned total;
+}avm_table;
+
+
+typedef enum avm_memcell_t{
+  number_m ,
+  string_m ,
+  bool_m ,
+  table_m ,
+  userfunc_m ,
+  libfunc_m ,
+  nil_m ,
+  undef_m 
+}avm_memcell_t;
+
+struct avm_memcell {
+  avm_memcell_t type;
+  union{
+    double numVal;
+    char* strVal;
+    unsigned char boolVal;
+    avm_table* tableVal;
+    unsigned funcVal;
+    char* libfuncVal;
+  }data;
+};
+
+struct avm_table_bucket {
+   avm_memcell key;
+   avm_memcell value;
+  avm_table_bucket* next;
+};
+
+
+
+
+
+
+
+
+
+
 int* numConsts;                             
 int numConstSize = 0;;
 char** stringConsts;
@@ -83,3 +139,28 @@ int userFuncSize = 0;
 void make_operand(expr* e , vmarg* argumnent);
 
 void emit_target(instr *i);
+
+avm_memcell stack[AVM_STACKSIZE];
+
+avm_table *avm_tablenew(void); 
+
+void avm_tabledestroy(avm_table* t); //8elei ilopoish
+
+avm_memcell* avm_tablegetelem(avm_memcell* key);//8elei ftia3imo
+
+void avm_tablesetelem(avm_memcell* key, avm_memcell* value); //8elei ftia3imo
+
+
+ static void avm_initstack(void);
+
+ void avm_tableincrefcounter(avm_table *t);
+
+ void avm_tabledecrefcounter(avm_table *t);
+
+ void avm_tablebucketsinit(avm_table_bucket **p);
+
+ void avm_memcellclear(avm_memcell* m);//8elei ilopoihsh
+
+ void avm_tablebucketsdestroy(avm_table_bucket **p);
+
+ void avm_tabledestroy(avm_table *t);
